3e31efb65fe0171dc7d41b9bd551b5da19a64978
Anna-Rebeka <rebeka.horecka@gmail.com>
2020-01-15 13:34:29 +0100

    zmena init-u
    text netreba pri vytváraní novej inštancie

--- messagebox.py
+++ messagebox.py
@@ -1,19 +1,16 @@
 import wx
- 
+
 class AlertBox():
-    def __init__(self, text):
-        self.text = text
-
-    def show(self, event):
-         wx.MessageBox(self.text, 'Upozornenie', wx.OK | wx.ICON_WARNING)
+    def show(self, text):
+         wx.MessageBox(text, 'Upozornenie', wx.OK | wx.ICON_WARNING)
 
 
-#volanie
-#   mb = AlertBox('lubovolna hlaska')
-#   XYbutton.Bind(wx.EVT_BUTTON, mb.show)
- 
+#volanie jednoducho
+#   ab = AlertBox()
+#   ab.show('lubovolny text')
 
-
-
-
-
+#volanie cez Bind
+#   self.ab = AlertBox()
+#   XYButton.Bind(wx.EVT_BUTTON, self.XYfunkcia)
+#   def XYfunkcia(self,event):
+#        self.ab.show('lubovolny text')
