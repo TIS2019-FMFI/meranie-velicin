65c129be153d84b8b0fbd98b3635a86b01083dc1
Savlar <33315801+Savlar@users.noreply.github.com>
2020-01-15 13:39:48 +0100

    deleted comments

--- messagebox.py
+++ messagebox.py
@@ -1,16 +1,6 @@
 import wx
 
 class AlertBox():
+    
     def show(self, text):
          wx.MessageBox(text, 'Upozornenie', wx.OK | wx.ICON_WARNING)
-
-
-#volanie jednoducho
-#   ab = AlertBox()
-#   ab.show('lubovolny text')
-
-#volanie cez Bind
-#   self.ab = AlertBox()
-#   XYButton.Bind(wx.EVT_BUTTON, self.XYfunkcia)
-#   def XYfunkcia(self,event):
-#        self.ab.show('lubovolny text')
