3bdeb46861f9717dfcd03a40ca497f530e9f66d0
keszeghova <daska.keszeghova@gmail.com>
2020-01-28 20:19:21 +0100

    method for time calculation

--- pipigraph.py
+++ pipigraph.py
@@ -6,14 +6,28 @@ import serial
 
 class PipiGraph:
 
-    def __init__(self):
+    def __init__(self, values):
         self.device = self.create_connection()
+        self.data = []
+        for x in values:
+            self.data.append((x[0], x[1][0]))
+        self.min_time = self.data[0][0]
+        self.max_time = self.data[-1][0]
 
     def create_connection(self):
         try:
             return serial.Serial('COM5', 9600, timeout=None, parity=serial.PARITY_NONE, rtscts=1)
         except serial.serialutil.SerialException:
             return False
+
+    def get_time(self, value):
+        """
+        value from device: 0 - 1023
+        """
+        return self.min_time + ((self.max_time - self.min_time) * (value / 1023.001))
+
+    def get_value(self, time):
+        ...
 
     def play(self, value):
         # values: 0 - 1023
