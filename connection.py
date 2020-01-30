a627113349d2ecb3ab3050f6eb585adb614c7ac5
Savlar <33315801+Savlar@users.noreply.github.com>
2020-01-30 11:19:43 +0100

    minor update

--- connection.py
+++ connection.py
@@ -48,7 +48,8 @@ class Connection:
             data_string = self.port.read(14)
         except serial.serialutil.SerialException:
             self.handler.window.cont_measurement = False
-        if self.kill and self.handler.window.cont_measurement is False:
+        if self.kill:
+            self.handler.window.cont_measurement = False
             return
         self.scheduler.enter(self.interval, 1, self.get_data)
         try:
